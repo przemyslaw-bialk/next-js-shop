164eba73c10f9a344ac85cf39b34634af92d5473
przemek <przemek.bialkwno@gmail.com>
2026-08-16 14:17:56 +0200

    refactoring

--- app/(SHOP)/components/contact/ContactForm.tsx
+++ app/(SHOP)/components/contact/ContactForm.tsx
@@ -1,15 +1,41 @@
-import { sendMessageFromUserAction } from "@/app/actions/message";
+"use client";
+import {
+  MessageActionState,
+  sendMessageFromUserAction,
+} from "@/app/actions/message";
+import { useActionState, useEffect } from "react";
+import { toast } from "sonner";
+
+const initialState: MessageActionState = {
+  status: null,
+};
 
 const ContactForm = () => {
+  const [state, action, isPending] = useActionState(
+    sendMessageFromUserAction,
+    initialState,
+  );
+
+  useEffect(() => {
+    if (state.status === "success") {
+      toast.success("message sent");
+    }
+    if (state.status === "error") {
+      toast.error("please fill in all the fields");
+    }
+  }, [state.status]);
+
   return (
-    <form action={sendMessageFromUserAction}>
+    <form action={action}>
       <label htmlFor="email">Email</label>
       <input type="email" placeholder="your mail" name="email" />
       <label htmlFor="subject">Subject</label>
       <input type="text" placeholder="enter the subject" name="subject" />
       <label htmlFor="message">Message</label>
       <textarea placeholder="write a message..." name="message"></textarea>
-      <button className="bg-neutral-800 text-white">send message</button>
+      <button className="bg-neutral-800 text-white">
+        {isPending ? "Sending..." : "Send message"}
+      </button>
     </form>
   );
 };
--- app/actions/message.ts
+++ app/actions/message.ts
@@ -1,11 +1,34 @@
 "use server";
 
 import { sendMessageFromUser } from "../services/message.service";
+export type MessageActionState = {
+  status: "success" | "error" | null;
+};
+export async function sendMessageFromUserAction(
+  prevState: MessageActionState,
+  formData: FormData,
+): Promise<MessageActionState> {
+  try {
+    const email = formData.get("email") as string;
+    const subject = formData.get("subject") as string;
+    const message = formData.get("message") as string;
 
-export async function sendMessageFromUserAction(formData: FormData) {
-  const email = formData.get("email") as string;
-  const subject = formData.get("subject") as string;
-  const message = formData.get("message") as string;
+    if (!email || !subject || !message) {
+      return {
+        status: "error",
+      };
+    }
 
-  await sendMessageFromUser(email, subject, message);
+    await sendMessageFromUser(email, subject, message);
+
+    return {
+      status: "success",
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      status: "error",
+    };
+  }
 }
